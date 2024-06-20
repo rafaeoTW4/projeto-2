de5b1031dde6c93c73cb76ab7c398352cd063950
rafaeoTW4 <rafael.marcal.parente@gmail.com>
2024-06-20 19:32:13 -0300

    Inicio do projeto exibindo a hora

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function mostrarDataHora(){
+    const date = new Date();
+
+
+    console.log(date.toLocaleString());
+
+    document.getElementById('date').innerText = date.toLocaleString();
+}
+
+
+mostrarDataHora();
